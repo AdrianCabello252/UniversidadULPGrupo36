fa33c1785f607e6eb3fd7c2d970bdb01f865c7f1
MariaVRomero <141704203+MariaVRomero@users.noreply.github.com>
2023-09-08 16:01:15 -0300

    clase conexion
    clase conexion

---
+++ UniversidadGrupo36/src/universidadgrupo36/AccesoADatos/Conexion.java
@@ -0,0 +1,41 @@
+
+package universidadgrupo36.AccesoADatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class Conexion {
+    
+    private static final String URL ="jdbc:mariadb://localhost/";
+    private static final String DB= "universidadulpgrupo36";
+    private static final String USUARIO= "root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    private Conexion (){}
+    
+    private static Connection getConnection(){
+    
+        if (connection == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                 
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "error al cargar drivers");
+            } catch (SQLException ex) {
+               JOptionPane.showMessageDialog(null, "error al conectarse");
+            }
+           
+        }
+        return connection;
+    
+    }
+    
+    
+}
---
+++ UniversidadGrupo36/src/universidadgrupo36/Entidades/Materia.java
@@ -0,0 +1,65 @@
+
+package universidadgrupo36.Entidades;
+
+
+public class Materia {
+    
+    private int idMateria;
+    private String nombre;
+    private int anio;
+    boolean estado;
+    
+    public Materia () {}
+
+    public Materia(int idMateria, String nombre, int anio, boolean estado) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.estado = estado;
+    }
+
+    public Materia(String nombre, int anio) {
+        this.nombre = nombre;
+        this.anio = anio;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anio=" + anio + '}';
+    }
+
+    
+    
+}
