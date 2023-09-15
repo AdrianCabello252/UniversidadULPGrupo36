a5ba1a15f87d78dd3d0a36174297dded24e5c974
MariaVRomero <141704203+MariaVRomero@users.noreply.github.com>
2023-09-15 15:44:14 -0300

    Metodos AlumnoVista

--- UniversidadGrupo36/src/universidadgrupo36/Vistas/AlumnoVista.java
+++ UniversidadGrupo36/src/universidadgrupo36/Vistas/AlumnoVista.java
@@ -4,6 +4,7 @@ package universidadgrupo36.Vistas;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import javax.swing.JOptionPane;
 import universidadgrupo36.AccesoADatos.AlumnoData;
 import universidadgrupo36.Entidades.Alumno;
 
@@ -15,7 +16,6 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
         initComponents();
     }
 
-  
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -34,8 +34,8 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
         jdcFechaNac = new com.toedter.calendar.JDateChooser();
         jbBuscar = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        jbEliminar = new javax.swing.JButton();
+        jbGuardar = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
@@ -75,17 +75,19 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
 
         jButton2.setText("NUEVO");
 
-        jButton3.setText("ELIMINAR");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        jbEliminar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jbEliminar.setText("ELIMINAR");
+        jbEliminar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                jbEliminarActionPerformed(evt);
             }
         });
 
-        jButton4.setText("GUARDAR");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        jbGuardar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jbGuardar.setText("GUARDAR");
+        jbGuardar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                jbGuardarActionPerformed(evt);
             }
         });
 
@@ -119,9 +121,7 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
                                             .addGroup(layout.createSequentialGroup()
                                                 .addGap(46, 46, 46)
                                                 .addComponent(jtdni, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                                .addComponent(jtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                            .addComponent(jtNombre, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))))
                                 .addGap(53, 53, 53)
                                 .addComponent(jbBuscar))
                             .addGroup(layout.createSequentialGroup()
@@ -134,16 +134,16 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
                                         .addGap(29, 29, 29)))
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jButton3)
+                                        .addComponent(jbEliminar)
                                         .addGap(25, 25, 25)
-                                        .addComponent(jButton4)
+                                        .addComponent(jbGuardar)
                                         .addGap(26, 26, 26)
                                         .addComponent(jButton5))
                                     .addComponent(jdcFechaNac, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(154, 154, 154)
                         .addComponent(jLabel1)))
-                .addContainerGap(53, Short.MAX_VALUE))
+                .addContainerGap(29, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -170,11 +170,11 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel6)
                     .addComponent(jdcFechaNac, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 61, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
-                    .addComponent(jButton3)
-                    .addComponent(jButton4)
+                    .addComponent(jbEliminar)
+                    .addComponent(jbGuardar)
                     .addComponent(jButton5))
                 .addContainerGap())
         );
@@ -182,16 +182,26 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    
+    private void jbEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbEliminarActionPerformed
+        
+        int dni = Integer.parseInt(jtdni.getText());    
+        AlumnoData aluData = new AlumnoData();
+        Alumno alu= aluData.buscarAlumnoPorDni(dni);
+        int idAlumno = alu.getIdAlumno();
+        aluData.eliminarAlumno(idAlumno);
+        
+        jtApellido.setText("");
+        jtNombre.setText("");
+        jtdni.setText("");
+        jdcFechaNac.setCalendar(null);
+        jcbActivo.setSelected(false);
+    }//GEN-LAST:event_jbEliminarActionPerformed
 
     private void jbBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbBuscarActionPerformed
 
        int dni = Integer.parseInt(jtdni.getText());
-       
        AlumnoData aluData = new AlumnoData();
-
        Alumno alu= aluData.buscarAlumnoPorDni(dni);
        
        jtApellido.setText(alu.getApellido());
@@ -199,21 +209,28 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
        if (alu.isActivo()) {
        jcbActivo.setSelected(true);
        }
-//       jdcFechaNac.setDate(alu.nuevaFecha());
-//       
+//     jdcFechaNac.setDate(alu.nuevaFecha());
        jdcFechaNac.setDate(Date.from(alu.getFechaNac().atStartOfDay(ZoneId.systemDefault()).toInstant()));
 
     }//GEN-LAST:event_jbBuscarActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton4ActionPerformed
+    private void jbGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbGuardarActionPerformed
+    
+        int dni = Integer.parseInt(jtdni.getText());    
+        AlumnoData aluData = new AlumnoData();
+        Alumno alu= aluData.buscarAlumnoPorDni(dni);
+    
+        alu.setApellido(jtApellido.getText());
+        alu.setNombre(jtNombre.getText());
+        alu.setActivo(jcbActivo.isSelected());
+        alu.setFechaNac(jdcFechaNac.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        aluData.guardarAlumno(alu);
+
+    }//GEN-LAST:event_jbGuardarActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -222,6 +239,8 @@ public class AlumnoVista extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JButton jbBuscar;
+    private javax.swing.JButton jbEliminar;
+    private javax.swing.JButton jbGuardar;
     private javax.swing.JCheckBox jcbActivo;
     private com.toedter.calendar.JDateChooser jdcFechaNac;
     private javax.swing.JTextField jtApellido;
